e1c12f55e132eb401189a124074dea6e5c529f53
andudina <ndudina99@yandex.ru>
2023-03-12 19:01:23 +0700

    Lesson1

---
+++ Lesson1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Lesson1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите ваше имя:");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Привет, {name}!"); 
+    }
+
+}
+
